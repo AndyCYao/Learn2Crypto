f7497af12ec5f3d383ef25acdba0ff7a94b5ae5f
Andy Y <andyyao3@gmail.com>
2018-08-08 15:17:40 -0700

    Finished problem 4 -persistence

--- bank.js
+++ bank.js
@@ -1,11 +1,25 @@
 // bank.js
+
 var jsonStream = require('duplex-json-stream')
 var net = require('net')
 
-var log = []
-var currentBalance = 0
+const fs = require('fs');
+// var log = []
+// fs.open('transactions.json', 'r', (err, fd) => {
+//   if (err) throw err;
+//   fs.close(fd, (err) => {
+//     if (err) throw err;
+//   });
 
-function reduceLog (balance, entry) {
+//   log = fd
+// });
+var log = require('./transactions.json');
+console.log(typeof(log))
+console.log(log)
+
+var currentBalance = log.reduce(reducer, 0)
+
+function reducer (balance, entry) {
   return balance + entry.amount
 }
 
@@ -13,32 +27,40 @@ var server = net.createServer(function (socket) {
   socket = jsonStream(socket)
   
   socket.on('data', function (msg) {
+    isSufficient = true
     switch(msg["cmd"]){
       case 'deposit':
         currentBalance += msg.amount
         log.push(msg)
-        var payload = {cmd : 'balance', balance : log.reduce(reduceLog, 0)}
+        // var payload = {cmd : 'balance', balance : log.reduce(reduceLog, 0)}
         break
       case 'balance':
-        var payload = {cmd : 'balance', balance : log.reduce(reduceLog, 0)}
+        // var payload = {cmd : 'balance', balance : log.reduce(reduceLog, 0)}
         break
       case 'withdraw':
         if(currentBalance >= msg.amount){
           currentBalance -= msg.amount
           msg.amount = -1 * msg.amount
           log.push(msg)
-          var payload = {cmd : 'balance', balance : log.reduce(reduceLog, 0)}
+          
         }
         else{
-          var payload = "Insufficient Fund"
+          isSufficient = false
         }
 
         break
       default:
         break
     }
+    if(isSufficient){
+      var payload = {cmd : 'balance', balance : log.reduce(reducer, 0)}
+      fs.writeFile('transactions.json', JSON.stringify(log, null, 1), (error) => { /* handle error */ })
+    } 
+    else{
+      var payload = "Insufficient fund"
+    }
     socket.write(payload)
   })
 })
-
+//server.end(JSON.stringify(log))
 server.listen(3876)
