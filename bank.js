fd93f1f448371267990df0a8d6bc316a110295a6
Andy Y <andyyao3@gmail.com>
2018-08-08 01:05:20 -0700

    finished problem 2

--- bank.js
+++ bank.js
@@ -1,32 +1,29 @@
 // bank.js
 var jsonStream = require('duplex-json-stream')
 var net = require('net')
-var m_Balance = 0
+
 var log = []
 
-const reducer = (accumulator , currentValue) => accumulator["amount"] + currentValue;
+function reduceLog (balance, entry) {
+  return balance + entry.amount
+}
 
 var server = net.createServer(function (socket) {
   socket = jsonStream(socket)
-  console.log("Entered Bank")
   
   socket.on('data', function (msg) {
     switch(msg["cmd"]){
       case 'deposit':
-      m_Balance +=  parseInt(msg["amount"])
-      log.push(msg)
-      m_newBalance = log.reduce(reducer); 
-      break
+        log.push(msg)
+        break
       case 'balance':
-      break
+        break
       default:
-      break
+        break
     }
     
-    var payload = {cmd : 'balance', balance : m_Balance}
+    var payload = {cmd : 'balance', balance : log.reduce(reduceLog, 0)}
     socket.write(payload)
-    //console.log(log)
-    console.log(m_newBalance)
   })
 })
 
--- teller.js
+++ teller.js
@@ -8,17 +8,13 @@ client.on('data', function (msg) {
   console.log('Teller received:', msg)
 })
 
-// process.argv.forEach(function(val, index, array) {
-//   console.log(index+ ': ' + val);
-// })
-
 var args = process.argv
 // console.log(args[2] + args[3])
-
 switch(args[2]){
 
   case 'deposit':
-    var payload = {cmd: 'deposit', amount: args[3]}
+    amount = parseFloat(args[3])
+    var payload = {cmd: 'deposit', amount: amount}
   break 
   case 'balance':
     var payload = {cmd: 'balance'}
